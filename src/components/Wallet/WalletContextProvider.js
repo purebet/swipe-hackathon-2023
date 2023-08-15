4c3c9092372e7e2cc56305afc7775b10fd825178
Wesley Bian <wesleybian@gmail.com>
2023-08-14 19:48:31 -0700

    wallet rpc fix?

--- src/components/Wallet/WalletContextProvider.js
+++ src/components/Wallet/WalletContextProvider.js
@@ -23,14 +23,8 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider = ({ children }) => {
     const network = process.env.REACT_APP_WALLET_ADAPTER_NETWORK == 'dev' ? WalletAdapterNetwork.Devnet : WalletAdapterNetwork.Mainnet;
-    //quicknode hopefully works better
-    //const endpoint = useMemo(() => "https://spring-frosty-snowflake.solana-mainnet.discover.quiknode.pro/5584f3ace79637af8f83a6f135554af9e0f0ffca/", [network]);
-    
-    const endpoint = useMemo(() => "https://rpc.helius.xyz/?api-key=2f6391bc-b23c-43ff-aac4-a04fc3112397", [network]);
-//use this for production
-    //const endpoint = useMemo(() => "https://solana-mainnet.g.alchemy.com/v2/GatLx4SVsTijZoDo9WJHKMos3lxr5jVX", [network]); //clusterApiUrl(network)
-    //use this for testing
-    //const endpoint = useMemo(() => "https://solana-mainnet.g.alchemy.com/v2/7ZjtJXlqW6cCksjr6I0aXETPeoSHzEWn", [network]);
+      
+    const endpoint = useMemo(() => process.env.REACT_APP_WALLET_ADAPTER_NETWORK, [network]);
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
